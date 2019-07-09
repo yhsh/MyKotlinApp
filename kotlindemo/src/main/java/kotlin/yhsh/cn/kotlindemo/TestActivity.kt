84ea1ace156f2142c7900d3389de9f10d32db032
zhengcong <zhengcong@reapal.com>
2019-07-09 19:57:38 +0800

    创建test文件

---
+++ kotlindemo/src/main/java/kotlin/yhsh/cn/kotlindemo/TestActivity.kt
@@ -0,0 +1,14 @@
+package kotlin.yhsh.cn.kotlindemo
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * 创建的test文件
+ */
+class TestActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+    }
+}
